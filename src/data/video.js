0c7208e7b6ec2bcaef90c51cf08ff049e51c4f62
saurab09876 <sauravsharma09876@gmail.com>
2026-05-14 11:42:58 +0530

    more video added

--- src/data/video.js
+++ src/data/video.js
@@ -18,6 +18,130 @@ const videos = [
     videoUrl:
       "https://www.w3schools.com/html/movie.mp4",
   },
+  {
+  },
+  {
+    id: 10,
+    title: "Flower Video 10",
+    thumbnail: "https://picsum.photos/400/250?random=10",
+    videoUrl: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+  },
+
+  {
+    id: 11,
+    title: "Big Buck Bunny 11",
+    thumbnail: "https://picsum.photos/400/250?random=11",
+    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+  },
+  {
+    id: 12,
+    title: "Sample Video 12",
+    thumbnail: "https://picsum.photos/400/250?random=12",
+    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-20s.mp4",
+  },
+  {
+    id: 13,
+    title: "Flower Video 13",
+    thumbnail: "https://picsum.photos/400/250?random=13",
+    videoUrl: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+  },
+  {
+    id: 14,
+    title: "Big Buck Bunny 14",
+    thumbnail: "https://picsum.photos/400/250?random=14",
+    videoUrl: "https://www.w3schools.com/html/movie.mp4",
+  },
+  {
+    id: 15,
+    title: "Sample Video 15",
+    thumbnail: "https://picsum.photos/400/250?random=15",
+    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-30s.mp4",
+  },
+  {
+    id: 17,
+    title: "Video 17",
+    thumbnail: "https://picsum.photos/400/250?random=17",
+    videoUrl: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+  },
+  {
+    id: 18,
+    title: "Video 18",
+    thumbnail: "https://picsum.photos/400/250?random=18",
+    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
+  },
+  {
+    id: 19,
+    title: "Video 19",
+    thumbnail: "https://picsum.photos/400/250?random=19",
+    videoUrl: "https://www.w3schools.com/html/movie.mp4",
+  },
+  {
+    id: 20,
+    title: "Video 20",
+    thumbnail: "https://picsum.photos/400/250?random=20",
+    videoUrl: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+  },
+
+  {
+    id: 21,
+    title: "Video 21",
+    thumbnail: "https://picsum.photos/400/250?random=21",
+    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+  },
+  {
+    id: 22,
+    title: "Video 22",
+    thumbnail: "https://picsum.photos/400/250?random=22",
+    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-10s.mp4",
+  },
+  {
+    id: 23,
+    title: "Video 23",
+    thumbnail: "https://picsum.photos/400/250?random=23",
+    videoUrl: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+  },
+  {
+    id: 24,
+    title: "Video 24",
+    thumbnail: "https://picsum.photos/400/250?random=24",
+    videoUrl: "https://www.w3schools.com/html/movie.mp4",
+  },
+  {
+    id: 25,
+    title: "Video 25",
+    thumbnail: "https://picsum.photos/400/250?random=25",
+    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-15s.mp4",
+  },
+   {
+    id: 96,
+    title: "Video 96",
+    thumbnail: "https://picsum.photos/400/250?random=96",
+    videoUrl: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+  },
+  {
+    id: 97,
+    title: "Video 97",
+    thumbnail: "https://picsum.photos/400/250?random=97",
+    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+  },
+  {
+    id: 98,
+    title: "Video 98",
+    thumbnail: "https://picsum.photos/400/250?random=98",
+    videoUrl: "https://samplelib.com/lib/preview/mp4/sample-20s.mp4",
+  },
+  {
+    id: 99,
+    title: "Video 99",
+    thumbnail: "https://picsum.photos/400/250?random=99",
+    videoUrl: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+  },
+  {
+    id: 100,
+    title: "Video 100",
+    thumbnail: "https://picsum.photos/400/250?random=100",
+    videoUrl: "https://www.w3schools.com/html/movie.mp4",
+  },
 ];
 
 export default videos;
